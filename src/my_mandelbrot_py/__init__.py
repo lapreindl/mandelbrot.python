9a7d92ee2eea28ecbfb9302b45f48abe2b5a08f1
Mathias Rieder <riederm@bachmann.at>
2026-03-10 07:44:42 +0000

    feat: allow package execution with -m

--- src/my_mandelbrot_py/__init__.py
+++ src/my_mandelbrot_py/__init__.py
@@ -3,3 +3,17 @@
 __version__ = "0.1.0"
 
 from .mandelbrot import draw_mandelbrot  # expose function
+
+
+# allow running with `python -m my_mandelbrot_py`
+
+
+def _run():
+    # avoid circular import when package is imported
+    from .cli import main
+
+    main()
+
+
+if __name__ == "__main__":
+    _run()
